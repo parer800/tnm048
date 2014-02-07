b6d9f13fbb79f42a00e5ee56a2414441faa3c179
Par Eriksson <parer800@student.liu.se>
2014-02-07 16:17:03 +0100

    deleted commas in country name, generated bugs in csv file

--- js/main.js
+++ js/main.js
@@ -3,3 +3,35 @@
 d3.csv("data/Total_Petroleum_Consumption_(Thousand_Barrels_Per_Day).csv", function(error, data) {
     self.data = data;
 });
+
+
+/* Knockout setup */
+var viewModel = {
+	foo : ko.observable("observer")
+};
+
+function Rect(){
+	var self = this;
+	self.x = ko.observable(0);
+	self.y = ko.observable(0);
+	self.w = ko.observable(100);
+	self.h = ko.observable(100);
+	self.name = ko.observable(makeName());
+};
+function ViewModel(){
+	var self = this;
+	self.rects = ko.observableArray([]);
+	self.addRect = function(){
+		self.rects.push(new Rect(self));
+	};
+};
+ /*----------------------------------------------------------------------- */
+
+ /* D3 linker */
+ var rects = d3.select("#svg")
+ 	.selectAll(rect)
+ 	.data(d, function (d){ return d.name(); });
+
+ rects.enter()
+ 	.append("rect")
+ 	.attr("id", function (d) {return d.name();});
